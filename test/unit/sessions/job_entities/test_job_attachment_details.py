1b2b17bd6161713f2099c990e51d220fc1569d03
Gahyun Suh <132245153+gahyusuh@users.noreply.github.com>
2023-10-13 12:25:50 -0500

    chore: add unit tests for input validation (#55)
    - `BatchGetJobEntity` API response data validation
    - `ConfigFile` model validation (pydantic)
    - `Capabilities` model validation (pydantic)
    
    Signed-off-by: Gahyun Suh <132245153+gahyusuh@users.noreply.github.com>
    Signed-off-by: Graeme McHale <gmchale@amazon.com>

---
+++ test/unit/sessions/job_entities/test_job_attachment_details.py
@@ -0,0 +1,196 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+from typing import Any
+import pytest
+
+from deadline.job_attachments.models import JobAttachmentsFileSystem
+from deadline_worker_agent.sessions.job_entities.job_attachment_details import JobAttachmentDetails
+
+
+@pytest.mark.parametrize("loading_method", [e.value for e in JobAttachmentsFileSystem])
+def test_asset_loading_method(loading_method):
+    """Test that the loading method is read from the boto data into JobAttachmentDetails"""
+    entity_obj = JobAttachmentDetails.from_boto(
+        job_attachments_details_data={
+            "jobId": "job-0000",
+            "attachments": {
+                "manifests": [],
+                "fileSystem": loading_method,
+            },
+        },
+    )
+
+    assert entity_obj is not None
+    assert entity_obj.job_attachments_file_system == loading_method
+
+
+def test_asset_loading_method_default():
+    """Test that the loading method is set to default when not included in boto data"""
+    entity_obj = JobAttachmentDetails.from_boto(
+        job_attachments_details_data={
+            "jobId": "job-0000",
+            "attachments": {
+                "manifests": [],
+            },
+        },
+    )
+
+    assert entity_obj is not None
+    assert entity_obj.job_attachments_file_system == JobAttachmentsFileSystem.COPIED
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": [],
+                },
+            },
+            id="only required fields with empty manifests list",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": [
+                        {
+                            "rootPath": "/myroot",
+                            "rootPathFormat": "posix",
+                        }
+                    ],
+                },
+            },
+            id="only required fields",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "stepId": "step-0000",
+                "attachments": {
+                    "manifests": [
+                        {
+                            "fileSystemLocationName": "mylocation1",
+                            "rootPath": "/myroot",
+                            "rootPathFormat": "posix",
+                            "outputRelativeDirectories": [],
+                            "inputManifestPath": "farm-0000/queue-0000/Inputs/0000/input1.xxh128",
+                            "inputManifestHash": "hash1",
+                        },
+                        {
+                            "fileSystemLocationName": "mylocation2",
+                            "rootPath": "/myroot",
+                            "rootPathFormat": "posix",
+                            "outputRelativeDirectories": ["./output1", "./output2"],
+                            "inputManifestPath": "farm-0000/queue-0000/Inputs/0000/input2.xxh128",
+                            "inputManifestHash": "hash2",
+                        },
+                    ],
+                    "fileSystem": "copied",
+                },
+            },
+            id="all fields",
+        ),
+    ],
+)
+def test_input_validation_success(data: dict[str, Any]) -> None:
+    """Test that validate_entity_data() can successfully handle valid input data."""
+    JobAttachmentDetails.validate_entity_data(entity_data=data)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(
+            {
+                "attachments": {
+                    "manifests": [],
+                    "fileSystem": "copied",
+                },
+            },
+            id="missing jobId",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+            },
+            id="missing attachments",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "fileSystem": "copied",
+                },
+            },
+            id="invalid attachments - missing manifests",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": [
+                        {
+                            "fileSystemLocationName": "mylocation",
+                            "rootPath": "myroot",
+                        },
+                    ],
+                },
+            },
+            id="invalid manifests - missing rootPathFormat",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": {},
+                },
+            },
+            id="invalid manifests - not list",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "stepId": "step-0000",
+                "attachments": {
+                    "manifests": [
+                        {
+                            "fileSystemLocationName": "mylocation1",
+                            "rootPath": "/myroot",
+                            "rootPathFormat": "posix",
+                            "outputRelativeDirectories": "./output",
+                        },
+                    ],
+                    "fileSystem": "copied",
+                },
+            },
+            id="invalid outputRelativeDirectories - not list",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": [],
+                },
+                "unknown": "unknown",
+            },
+            id="unknown field",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "attachments": {
+                    "manifests": [],
+                    "unknown": "unknown",
+                },
+            },
+            id="unknown field in attachments",
+        ),
+    ],
+)
+def test_input_validation_failure(data: dict[str, Any]) -> None:
+    """Test that validate_entity_data() raises a ValueError when invalid input data is provided."""
+    with pytest.raises(ValueError):
+        JobAttachmentDetails.validate_entity_data(entity_data=data)
---
+++ test/unit/sessions/job_entities/test_job_details.py
@@ -0,0 +1,269 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+from typing import Any
+import pytest
+
+from deadline_worker_agent.sessions.job_entities.job_details import JobDetails
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+            },
+            id="only required fields",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "parameters": {
+                    "param1": {
+                        "string": "param1value",
+                    },
+                    "param2": {
+                        "path": "param2value",
+                    },
+                },
+            },
+            id="valid parameters",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "pathMappingRules": [],
+            },
+            id="valid pathMappingRules - empty list",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "pathMappingRules": [
+                    {
+                        "sourcePathFormat": "posix",
+                        "sourcePath": "/source/path",
+                        "destinationPath": "/destination/path",
+                    },
+                ],
+            },
+            id="valid pathMappingRules",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobRunAsUser": {},
+            },
+            id="valid jobRunAsUser - empty dict",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobRunAsUser": {
+                    "posix": {
+                        "user": "user1",
+                        "group": "group1",
+                    },
+                },
+            },
+            id="valid jobRunAsUser",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobAttachmentSettings": {
+                    "s3BucketName": "mybucket",
+                    "rootPrefix": "myprefix",
+                },
+            },
+            id="valid jobAttachmentSettings",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "osUser": "",
+                "parameters": {
+                    "param1": {
+                        "string": "param1value",
+                    },
+                    "param2": {
+                        "path": "param2value",
+                    },
+                },
+                "pathMappingRules": [
+                    {
+                        "sourcePathFormat": "posix",
+                        "sourcePath": "/source/path",
+                        "destinationPath": "/destination/path",
+                    },
+                ],
+                "jobRunAsUser": {
+                    "posix": {
+                        "user": "user1",
+                        "group": "group1",
+                    },
+                },
+                "jobAttachmentSettings": {
+                    "s3BucketName": "mybucket",
+                    "rootPrefix": "myprefix",
+                },
+                "queueRoleArn": "0000:role/myqueuerole",
+            },
+            id="all fields",
+        ),
+    ],
+)
+def test_input_validation_success(data: dict[str, Any]) -> None:
+    """Test that validate_entity_data() can successfully handle valid input data."""
+    JobDetails.validate_entity_data(entity_data=data)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+            },
+            id="missing schemaVersion",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+            },
+            id="missing logGroupName",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "parameters": {
+                    "param1": {"string": "param1value", "anotherKey": "value"},
+                },
+            },
+            id="invalid parameters - a parameter has two keys.",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "parameters": {
+                    "param1": {
+                        "unknownType": "param1value",
+                    },
+                },
+            },
+            id="invalid parameters - a type key is unknown type.",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "pathMappingRules": [
+                    {
+                        "sourcePath": "/source/path",
+                        "destinationPath": "/destination/path",
+                    },
+                ],
+            },
+            id="invalid pathMappingRules - missing sourcePathFormat",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "pathMappingRules": {},
+            },
+            id="invalid pathMappingRules - not list",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobRunAsUser": {
+                    "posix": {
+                        "user": "user1",
+                    },
+                },
+            },
+            id="invalid jobRunAsUser - missing group",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobRunAsUser": [
+                    {
+                        "posix": {
+                            "user": "user1",
+                        },
+                    }
+                ],
+            },
+            id="invalid jobRunAsUser - not dict",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "jobAttachmentSettings": {
+                    "s3BucketName": "mybucket",
+                },
+            },
+            id="invalid jobAttachmentSettings - missing rootPrefix",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "unknown": "field",
+            },
+            id="unknown field",
+        ),
+        pytest.param(
+            {
+                "jobId": "job-0000",
+                "logGroupName": "/aws/deadline/queue-0000",
+                "schemaVersion": "jobtemplate-0000-00",
+                "pathMappingRules": [
+                    {
+                        "sourcePath": "/source/path",
+                        "destinationPath": "/destination/path",
+                        "unknown": "unknown",
+                    },
+                ],
+            },
+            id="unknown field in pathMappingRules",
+        ),
+    ],
+)
+def test_input_validation_failure(data: dict[str, Any]) -> None:
+    """Test that validate_entity_data() raises a ValueError when invalid input data is provided."""
+    with pytest.raises(ValueError):
+        JobDetails.validate_entity_data(entity_data=data)
