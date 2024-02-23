106969b993aeb82fe56498525a1790542912cb81
Graeme McHale <gmchale@amazon.com>
2024-02-23 15:20:19 -0800

    feat: session cleanup on windows
    Signed-off-by: Graeme McHale <gmchale@amazon.com>

--- src/deadline_worker_agent/scheduler/session_cleanup.py
+++ src/deadline_worker_agent/scheduler/session_cleanup.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 
 import subprocess
 import os
+import getpass
 from threading import Lock
 
 from openjd.sessions import SessionUser, PosixSessionUser, WindowsSessionUser
-from typing import cast
 from .log import LOGGER
 from ..sessions import Session
 
@@ -38,25 +38,16 @@ class SessionUserCleanupManager:
         self._user_session_map = {}
         self._cleanup_session_user_processes = cleanup_session_user_processes
 
-    def _get_user(self, session: Session):
-        if os.name == "posix":
-            posix_user: PosixSessionUser = cast(PosixSessionUser, session.os_user)
-            return posix_user.user
-        else:
-            windows_user: WindowsSessionUser = cast(WindowsSessionUser, session._os_user)
-            return windows_user.user
-
     def register(self, session: Session):
         if session.os_user is None:
             return
 
         with self._user_session_map_lock:
-            user_name = self._get_user(session)
+            user_name = session.os_user.user
             session_dict = self._user_session_map.get(user_name, None)
             if session_dict is None:
                 session_dict = {}
                 self._user_session_map[user_name] = session_dict
-
             session_dict[session.id] = session
 
     def deregister(self, session: Session):
@@ -64,7 +55,7 @@ class SessionUserCleanupManager:
             return
 
         with self._user_session_map_lock:
-            user_name = self._get_user(session)
+            user_name = session.os_user.user
             session_dict = self._user_session_map.get(user_name, None)
             if session_dict is None:
                 return
@@ -90,23 +81,13 @@ class SessionUserCleanupManager:
                 logger.warn(f"Failed to stop session user processes: {e}")
 
     @staticmethod
-    def cleanup_session_user_processes(user: SessionUser):
-        if not isinstance(user, PosixSessionUser):
-            # TODO: Windows support
-            logger.warning(
-                "Stopping session user processes will be skipped because this feature is only supported on POSIX systems"
-            )
-            raise NotImplementedError("Windows not supported")
+    def _is_current_user(user: SessionUser):
+        current_user = getpass.getuser()
+        return user.user == current_user
 
-        # Check that the session user isn't the current user (agent user)
-        current_user = subprocess.check_output(["/usr/bin/whoami"], text=True).strip()
-        if current_user == user.user:
-            logger.info(
-                f"Skipping cleaning up processes because the session user matches the agent user '{current_user}'"
-            )
-            return
-
-        logger.info(f"Cleaning up remaining session user processes for '{user.user}'")
+    @staticmethod
+    def _posix_cleanup_user_processes(user: SessionUser):
+        assert isinstance(user, PosixSessionUser)
         try:
             pkill_result = subprocess.run(
                 args=["sudo", "-u", user.user, "/usr/bin/pkill", "-eU", user.user],
@@ -130,3 +111,59 @@ class SessionUserCleanupManager:
             #  etc.
             pkill_output = "\n".join([pkill_result.stdout, pkill_result.stderr]).rstrip()
             logger.info(f"Stopped processes:\n{pkill_output}")
+
+    @staticmethod
+    def _windows_cleanup_user_processes(user: SessionUser):
+        import psutil
+        import win32api
+        import win32con
+
+        assert isinstance(user, WindowsSessionUser)
+        username = user.user
+        processes = []
+
+        for proc in psutil.process_iter(["pid", "username"]):
+            proc_info = proc.info  # type: ignore
+            try:
+                if process_owner_with_domain := proc_info.get("username"):
+                    # username is always 'host\user' or 'domain\user' so split it
+                    domain, process_owner = process_owner_with_domain.split("\\")
+                    if username.lower() == process_owner:
+                        processes.append(proc_info)
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                # Finding a process we can't get info for is not concerning
+                pass
+
+        if processes:
+            for proc_info in processes:
+                try:
+                    # Administrators can stop any process. Non-administrators can stop
+                    # any process they spawn regardless of the user it is spawned as
+                    pid = proc_info["pid"]
+                    process_handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, pid)
+                    win32api.TerminateProcess(process_handle, 0)
+                    win32api.CloseHandle(process_handle)
+                    logger.info(f"Stopped process PID: {pid} running as {username}")
+                except Exception as e:
+                    logger.warning(
+                        f"Failed to stop process PID: {pid} running as {username}: {str(e)}"
+                    )
+        else:
+            logger.info(f"No processes stopped because none were found running as '{user.user}'")
+
+    @staticmethod
+    def cleanup_session_user_processes(user: SessionUser):
+        # Check that the session user isn't the current user (agent user)
+        current_user = getpass.getuser()
+        if current_user == user.user:
+            logger.info(
+                f"Skipping cleaning up processes because the session user matches the agent user '{current_user}'"
+            )
+            return
+
+        logger.info(f"Cleaning up remaining session user processes for '{user.user}'")
+
+        if os.name == "posix":
+            SessionUserCleanupManager._posix_cleanup_user_processes(user)
+        else:
+            SessionUserCleanupManager._windows_cleanup_user_processes(user)
