f1afc8a09c34d8e4eeece4ebd29fbcab14a54c93
AlexTranAmz <167144297+AlexTranAmz@users.noreply.github.com>
2024-08-08 13:38:40 -0700

    test:Add test to verify install give shutdown permission to WA user (#375)
    * test:Add test to verify install give shutdown permission to WA user
    
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>
    
    * fix:change so test only run on linux
    
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>

---
+++ test/e2e/linux/test_installer.py
@@ -0,0 +1,26 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+"""
+This test module contains tests that verify the Installer's behavior by submitting commands to the
+Deadline Cloud worker and checking that the result/output of the worker agent is as we expect it.
+"""
+import pytest
+import logging
+from deadline_test_fixtures import EC2InstanceWorker
+
+LOG = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize("operating_system", ["linux"], indirect=True)
+class TestInstaller:
+    def test_installer_shutdown_permission(
+        self,
+        session_worker: EC2InstanceWorker,
+    ) -> None:
+
+        cmd_result = session_worker.send_command(
+            "egrep \
+                '^deadline-worker ALL=\(root\) NOPASSWD: /usr/sbin/shutdown now$' \
+                /etc/sudoers.d/deadline-worker-shutdown"
+        )
+
+        assert cmd_result.exit_code == 0, f"Shutdown WA permission do not exist: {cmd_result}"
