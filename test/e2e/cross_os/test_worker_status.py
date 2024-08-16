d2c0fab1ffe646e6fc3cdbbd05267d4bd57a416b
Yutong Li <52769999+YutongLi291@users.noreply.github.com>
2024-08-15 18:10:32 -0700

    test: refactor test for worker status to use ssm command
    Signed-off-by: Yutong Li <52769999+YutongLi291@users.noreply.github.com>

--- test/e2e/cross_os/test_worker_status.py
+++ test/e2e/cross_os/test_worker_status.py
@@ -3,15 +3,15 @@
 This test module contains tests that verify the Worker agent's behavior by starting/stopping the Worker,
 and making sure that the status of the Worker is that of what we expect.
 """
+import logging
 import os
 import pytest
 from typing import Any, Dict
-from deadline_worker_agent.api_models import (
-    WorkerStatus,
-)
 import backoff
 from deadline_test_fixtures import DeadlineClient, EC2InstanceWorker
 import pytest
+
+LOG = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize("operating_system", [os.environ["OPERATING_SYSTEM"]], indirect=True)
@@ -47,19 +47,15 @@ class TestWorkerStatus:
 
         assert is_worker_started()
 
-        deadline_client.update_worker(
-            farmId=deadline_resources.farm.id,
-            fleetId=deadline_resources.fleet.id,
-            workerId=function_worker.worker_id,
-            status=WorkerStatus.STOPPED,
-        )
+        function_worker.stop_worker_service()
 
         @backoff.on_predicate(
             wait_gen=backoff.constant,
-            max_time=60,
+            max_time=180,
             interval=10,
         )
         def is_worker_stopped() -> bool:
+            LOG.info(f"Checking whether {function_worker.worker_id} is stopped")
             get_worker_response: Dict[str, Any] = deadline_client.get_worker(
                 farmId=deadline_resources.farm.id,
                 fleetId=deadline_resources.fleet.id,
