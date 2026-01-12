62610a23085c7742c52c5be7a82773cc5ec787d6
Evan Spearman <evans@amazon.com>
2026-01-12 15:42:50 -0600

    chore: Fixed scripts/assume_role_to_env.sh not functioning
    Signed-off-by: Evan Spearman <evans@amazon.com>

--- scripts/assume_role_to_env.sh
+++ scripts/assume_role_to_env.sh
@@ -4,13 +4,13 @@
 if ! which jq 2>&1 > /dev/null
 then
     echo "ERROR: jq must be installed."
-    exit 1
+    return 1
 fi
 
 if test $# -lt 1
 then
     echo "Usage: $0 <IAM Role Arn>"
-    exit 1
+    return 1
 fi
 
 unset AWS_ACCESS_KEY_ID
@@ -18,13 +18,28 @@ unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
 ASSUME_ROLE=$(aws sts assume-role --role-arn "$1" --role-session-name WorkerAgentAssumeRole)
+export ASSUME_ROLE
+
 AWS_ACCESS_KEY_ID=$(printenv ASSUME_ROLE | jq -r '.Credentials''.AccessKeyId')
 AWS_SECRET_ACCESS_KEY=$(printenv ASSUME_ROLE | jq -r '.Credentials''.SecretAccessKey')
 AWS_SESSION_TOKEN=$(printenv ASSUME_ROLE | jq -r '.Credentials''.SessionToken')
 
-export ASSUME_ROLE
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
 
 unset ASSUME_ROLE
+
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+    echo "ERROR: failed to set AWS_ACCESS_KEY_ID"
+    return 1
+fi
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+    echo "ERROR: failed to set AWS_SECRET_ACCESS_KEY"
+    return 1
+fi
+if [[ -z "$AWS_SESSION_TOKEN" ]]; then
+    echo "ERROR: failed to set AWS_SESSION_TOKEN"
+    return 1
+fi
+
